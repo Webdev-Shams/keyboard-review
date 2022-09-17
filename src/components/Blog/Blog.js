2002e447e9bceff98a4bf0be01e26f4a57ebbe6b
Webdev-Shams <webdev.shams@gmail.com>
2022-09-18 00:36:54 +0600

    Articles added to blog

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = () => {
     return (
-        <div>
-            
+        <div className='blogsContainer'>
+            <div className='mt-9'>
+                <h1 className='font-semibold text-2xl md:text-3xl lg:text-4xl'>What is context API?</h1>
+                <p>Context API refers to the way of passing data through the component tree instead of passing props down manually at each and every level.</p>
+            </div>
+            <div className='mt-9'>
+                <h1 className='font-semibold text-2xl md:text-3xl lg:text-4xl'>What are Semantic Tags?</h1>
+                <p>The Semantic Tags refer to the tags that brings meaning to an HTML page rather than just presentation. It makes HTML more understandable by better indicating the different sections and layout of web pages.</p>
+            </div>
+            <div className='mt-9'>
+                <h1 className='font-semibold text-2xl md:text-3xl lg:text-4xl'>What are the differences between inline and inline block elements?</h1>
+                <p>Compared to display:inline, the major difference is that inline-block allows to set a width and height on the element. Also, with display:inline, top and bottom margins & paddings are not respected, and with display: inline-block they are.</p>
+            </div>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Home from '../Home/Home';
+import './notFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1 className='font-sans text-5xl'>Page not found!!!</h1>
+        <div className='notFoundContainer grid grid-cols-1 items-center font-bold'>
+            <h1 className='notFoundTxt font-sans text-5xl'>PAGE NOT FOUND!<br></br>
+            <Link to="/Home">
+                <span>proceed to homepage--></span>
+            </Link>
+            </h1>
+            
         </div>
     );
 };
